1ca6746c9787cf31080f7f641f2289d3acda190c
liuxun <553798508@qq.com>
2017-09-04 00:32:44 +0800

    numpy

--- numpydemo/demo2.py
+++ numpydemo/demo2.py
@@ -37,7 +37,7 @@ print(arr3d[0])
 print(arr3d[1,0])
 print(arr3d[:2,1:])
 print(arr3d[:,1:])
-
+print('----------------------布尔型索引--------------------------')
 #布尔型索引 比较运算将会产生一个布尔型的数组
 names=np.array(['Bob','Joe','Will','Bob','Will'])
 print(names=='Bob')
@@ -47,4 +47,33 @@ print(names[0:2])
 data=randn(5,4)
 print(data)
 print(data[names=='Bob',:]) #可以用于数组索引，布尔数组的长度必须跟被索引的轴长度一致。查找为True的行
+data[data<0]=0      #通过布尔数组将data中的所有负值设置为0
+print(data)
 
+print('----------------------花式索引--------------------------')
+data1=np.empty((8,4));
+for n in range(8):
+    data1[n]=n
+print(data1)
+#选取特定行子集，只需要传入一个列表或者ndarray
+print(data1[[1,2,3]])
+dd=np.arange(32).reshape(8,4)
+print(dd)
+print(dd[np.ix_([1,5,7,2],[0,3,1,2])]) #ix_将数组转换为一个方形区域的索引器
+print(dd[np.ix_([0,1,2],[0,1,2,3])]) #第一个[]是选择行的数组、第二个是列的索引
+
+#数组的轴对换
+print('----------------------数组的轴对换--------------------------')
+arr=np.arange(12).reshape(3,4)
+print(arr)
+print(arr.T) #行转列
+
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr)
+print(arr.T)
+print(np.dot(arr.T,arr));
+
+arr=np.arange(16).reshape(2,2,4)
+print(arr)
+print(arr.transpose((1,0,2)))
+print(arr.swapaxes(1,2)) #多维数组行转列
