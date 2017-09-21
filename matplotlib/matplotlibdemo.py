d198bbff1458b9667470e4dfa89e17d2b8c08a94
liuxun <liuxun@jd.com>
2017-09-21 15:41:57 +0800

    python3数据分析实例-matplotlib图形绘制

--- matplotlib/matplotlibdemo.py
+++ matplotlib/matplotlibdemo.py
@@ -2,10 +2,53 @@
 
 import matplotlib.pyplot as plt
 from numpy.random import randn
-fig=plt.figure(2)
-ax1=fig.add_subplot(2,2,1)
-ax2=fig.add_subplot(2,2,2)
-ax3=fig.add_subplot(2,2,3)
-print(randn(50).cumsum())
-plt.plot(randn(50).cumsum(),'k--')
-plt.show()
+import numpy as np
+
+'''
+NumPy绘制图形
+'''
+
+def demo1():
+    fig = plt.figure(2)  # 指定编号
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax3 = fig.add_subplot(2, 2, 3)
+    print(randn(50).cumsum())
+    plt.plot(randn(50).cumsum(), 'k--')  # plt就会在最后一个用过的subplot上进行绘制，k--是线型选项，用于绘制黑色虚线
+    # 可以在指定的subplot上进行绘制
+    ax1.hist(randn(100), bins=20, color='k', alpha=0.3)  # 柱状图
+    ax2.scatter(np.arange(30), np.arange(30) + 3 * randn(30))  # 散点图
+    plt.show()
+def demo2():
+    '''
+    subplots可以创建一个新的figure，并返回一个含有已创建的subplot对象的Numpy数组
+    sharex 所有的subplot使用相同的X轴刻度
+    sharey 所有的subplot使用相同的Y轴刻度
+    '''
+    fig,axes=plt.subplots(2,2,sharex=True,sharey=True) #创建两行两列的图
+    for i in range(2):
+        for j in range(2):
+            axes[i,j].hist(randn(100), bins=20, color='k', alpha=0.3)  # 柱状图
+    plt.subplots_adjust(wspace=0,hspace=0) #图与图之间的宽度、高度的百分比
+    plt.show();
+def demo3():
+    fig = plt.figure()  # 指定编号
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    ax1.plot(randn(10).cumsum(),'g--')  #绘制绿色的虚线
+    ax2.plot(randn(10).cumsum(),color='g',linestyle='--',marker='o',label='steps-post')  #绘制绿色以及带有标记数据点的虚线
+    plt.show();
+def demo4():
+    fig=plt.figure();
+    ax=fig.add_subplot(111);
+    # ax.set_xticks([0,0.1,0.2,0.3,0.4]); #设置x轴刻度
+    # ax.set_yticks([0,0.1,0.2,0.3,0.4]) #设置y轴刻度
+    # ax.set_xticklabels(['a','b','c','d','e']); #x轴刻度标签
+    ax.set_title('my name tisck') #图标题
+    ax.set_xlabel('Stages');  #x轴标签
+    ax.plot(randn(1000).cumsum(),'r--',label='two') #添加数据
+    ax.plot(randn(1000).cumsum(),'k--',label='three') #添加数据
+    plt.savefig('firstpath.png') #保存图片到本地，还可以保存到StringIOs流中
+    plt.show();
+if __name__ == '__main__':
+    demo4();
---
+++ matplotlib/matplotlibdemo2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from numpy.random import randn
+import numpy as np
+from pandas import Series,DataFrame
+import pandas as pd
+'''
+pandas中绘制图
+
+'''
+def demo1():
+    s=Series(randn(10).cumsum(),index=np.arange(0,100,10))
+    s.plot();
+    plt.show()
+def demo2():
+    data=DataFrame(np.random.randn(10,4).cumsum(0),columns=['a','b','c','d'],index=np.arange(0,100,10))
+    data.plot(style='o--');
+    plt.show()
+'''
+柱状图绘制
+'''
+def demo3():
+    fig,axs=plt.subplots(2,1)
+    s=Series(np.random.randn(16),index=range(16))
+    s.plot(kind='bar',ax=axs[0],color='g',alpha=0.7); #kind图的形状，ax指定的图上进行绘制，color颜色，alpha透明度
+    s.plot(kind='barh', ax=axs[1], color='g', alpha=0.7);
+    plt.show()
+def demo4():
+    fig,axs=plt.subplots(2,1)
+    s=DataFrame(np.random.randn(6,4),index=list('abcdef'),columns=['one','two','three','four'])
+    s.plot(kind='bar',ax=axs[0],color='g',alpha=0.7); #kind图的形状，ax指定的图上进行绘制，color颜色，alpha透明度
+    s.plot(kind='barh', ax=axs[1], color='g', alpha=0.7);
+    plt.show()
+
+if __name__ == '__main__':
+    demo4();
