626c420136e7f3489a214a8db8a47d6695e1285f
刘勋 <xun2.liu@ly.com>
2020-07-15 14:52:45 +0800

    线性图

---
+++ matplotlib/fangja.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(40,40))
+
+plt.scatter([60, 72, 75, 80, 83, 87, 90, 93],[126, 151.2, 157.5, 168, 174.3, 180, 192.2,194],s=160)
+
+plt.show()
+
+
