1550a6c858fbd97953cc6a5476eb41babe609dfa
刘勋 <xun2.liu@ly.com>
2020-07-22 20:14:42 +0800

    线性回归

--- linear_regression/boston_linear.py
+++ linear_regression/boston_linear.py
@@ -7,7 +7,10 @@ from sklearn.linear_model import LinearRegression,SGDRegressor
 from sklearn.model_selection import train_test_split
 #导入标准化API
 from sklearn.preprocessing import StandardScaler
+#导入均方误差API
+from sklearn.metrics import mean_squared_error
 def bostonlinear():
+
     #1、获取数据
     lb=load_boston()
     # ##特征值
@@ -16,6 +19,7 @@ def bostonlinear():
     print(lb.target)
     #特征名称
     print(lb.feature_names)
+
 
     #2、分割数据集为训练集合测试集
     x_train,x_test,y_train,y_test=train_test_split(lb.data,lb.target,test_size=0.25)
@@ -33,11 +37,13 @@ def bostonlinear():
     #4、正规方程求解方式预测结果
     lr=LinearRegression()
     lr.fit(x_train,y_train)
-    ##查看回归系数
+    ##查看回归系数(每个特征的权值)
     print("回归系数",lr.coef_)
     #预测结果。因为预测的结果是标准化后的值。需要还原
-    x_predict=std_y.inverse_transform(lr.predict(x_test))
-    print("测试集房子价格预测结果：",x_predict)
+    y_predict=std_y.inverse_transform(lr.predict(x_test))
+    print("测试集房子价格预测结果：",y_predict)
 
+    #5、回归性能评估 真实值与预测值的均方误差
+    print("正规方程的均方误差值为：",mean_squared_error(std_y.inverse_transform(y_test),y_predict))
 if __name__ == "__main__":
     bostonlinear()
--- linear_regression/boston_sgd.py
+++ linear_regression/boston_sgd.py
@@ -7,6 +7,9 @@ from sklearn.linear_model import LinearRegression,SGDRegressor
 from sklearn.model_selection import train_test_split
 #导入标准化API
 from sklearn.preprocessing import StandardScaler
+#导入均方误差API
+from sklearn.metrics import mean_squared_error
+
 def bostonlinear():
     #1、获取数据
     lb=load_boston()
@@ -33,13 +36,14 @@ def bostonlinear():
     #4、正规方程求解方式预测结果
     sgd=SGDRegressor()
     sgd.fit(x_train,y_train)
-    ##查看回归系数
+    ##查看回归系数(每个特征的权值)
     print("回归系数",sgd.coef_)
     #预测结果。因为预测的结果是标准化后的值。需要还原
-    x_predict=std_y.inverse_transform(sgd.predict(x_test))
-    print("测试集房子价格预测结果：",x_predict)
+    y_predict=std_y.inverse_transform(sgd.predict(x_test))
+    print("测试集房子价格预测结果：",y_predict)
 
-    #5、回归性能评估
-    
+    #5、回归性能评估 真实值与预测值的均方误差
+    print("梯度下降的均方误差值为：",mean_squared_error(std_y.inverse_transform(y_test),y_predict))
+
 if __name__ == "__main__":
     bostonlinear()
