a125e7df0a4a71360fa1557ea6b469b11442e454
刘勋 <xun2.liu@ly.com>
2020-07-28 20:21:28 +0800

    房价散点图

--- matplotlib/fangja.py
+++ matplotlib/fangja.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
-
+'''
+房价散点图
+'''
 plt.figure(figsize=(40,40))
 
 plt.scatter([60, 72, 75, 80, 83, 87, 90, 93],[126, 151.2, 157.5, 168, 174.3, 180, 192.2,194],s=1000)
